99ef7bfd456538e9521eec8a36f79e7da32f1a28
prateekparkash1 <prateekparkash1@gmail.com>
2020-04-11 04:13:50 +0530

    removed placeholder in signin

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -5,8 +5,8 @@ import { signin, authenticate, isAuthenticated } from "../auth";
 
 const Signin = () => {
     const [values, setValues] = useState({
-        email: "prateek@gmail.com",
-        password: "prat12",
+        email: "",
+        password: "",
         error: "",
         loading: false,
         redirectToReferrer: false
